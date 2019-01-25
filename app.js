bb3dc5e4093c160f97f8585a56a772a711b2076e
aronela <aronela.lucaci@emanuel.ro>
2019-01-25 22:33:36 +0200

    Tema 3 - elementele unui array de string-uri

---
+++ app.js
@@ -0,0 +1,7 @@
+var arrString;
+arrString = ['C', 'A', 'E', 'B', 'D'];
+arrString.sort();
+console.log("Ordine alfabetica: ");
+for (var i = 0; i < arrString.length; i++) {
+    console.log(arrString[i]);
+}
